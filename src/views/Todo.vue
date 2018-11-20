bbeb2e9a096f4c65f2edc5850dcd684d4765f282
Ashok Rayal <ashokrayal27@gmail.com>
2018-11-20 11:44:38 +0530

    initial commit

---
+++ src/views/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1> Todo List</h1>
+        <ul>
+          <input v-model="newTask">
+          <button @click="addTask">ADD</button>
+          <li v-for="todo in todos">
+          {{todo}}
+          </li>
+        </ul>
+    </div>
+
+</template>
+<script>
+export default {
+  data: function() {
+    return ({
+      newTask: "",
+      todos: ["first task", "second task"]
+    })
+  },
+  methods:{
+    addTask() {
+      this.todos.push(this.newTask)
+    }
+  }
+};
+</script>
+
+
